9fe15882d26975510877d56cf7196be9e25884f7
Anthony Smith <anthony.smith1760@gmail.com>
2020-04-03 16:25:40 -0400

    Add files via upload

---
+++ jQuery Basics/jQuery Basics/main.js
@@ -0,0 +1,93 @@
+// $('#btn1').on('click',function () {
+//     $('#para1').toggle();
+// });
+
+
+// $('#btn2').on('click',function () {
+//     $('#para1').show();
+// });
+
+// $('#btn1').dblclick(function () {
+//     $('.para1').toggle();
+// });
+
+// $('#btn1').hover(function () {
+//     $('.para1').toggle();
+// });
+
+// $('#btn1').on('mousedown',function() {
+//     $('.para1').toggle();
+// });
+
+// $('#btn1').on('mouseup',function() {
+//     $('.para1').toggle();
+// });
+
+// $('#btn1').on('click',function (e) {
+//     // alert(e.currentTarget.id);
+//     // alert(e.currentTarget.innerHTML);
+//     // alert(e.currentTarget.className);
+// });
+
+    // $('input').focus(function() {
+    //     $(this).css('backgroundColor','pink');
+    // });
+
+    // $('input').blur(function() {
+    //     $(this).css('backgroundColor','white');
+    // });
+
+    // $('input').keyup(function(e) {
+    //     console.log(e.target.value);
+    // });
+
+    // $('select#gender').change(function(e) {
+    //     alert('e.target.value');
+    // });
+
+    // $('#form').submit(function(e) {
+    //     e.preventDefault();
+    //     var name = $('input#name').val();
+    //     console.log(name);
+    // });
+
+    // $('p.para1').css({color: "red", backgroundColor: '#ccc'});
+    // $('#btn1').click(function() {
+    //     $('p.para2').toggleClass('myclass');
+    // });
+
+    // $('#mydiv').html('<h3>Hello World</h3>');
+    // alert($('#mydiv').text());
+    // $('ul').append('<li>Append List Item</li>');
+    // $('ul').prepend('<li>Prepend List Item</li>');
+
+    // $('.para1').appendTo('.para2');
+    // $('.para1').prependTo('.para2');
+    // $('ul').before('<h4>Hello</h4>');
+    // $('ul').after('<h4>World</h4>');
+    // $('ul').empty();
+    // $('ul').detach();
+    // $('a').attr('target', '_blank');
+    // alert($('a').attr('href'));
+    // $('a').removeAttr('target');
+    // $('p').wrap('<h1>');
+    // $('p').wrapAll('<h1>');
+
+    // $('#newItem').keyup(function(e) {
+    //     var code = e.which;
+    //     if(code == 13) {
+    //         e.preventDefault();
+    //         $('ul').append('<li>'+e.target.value+'</li>');
+    //     }
+    // });
+
+    // // var myArr = ['Brad' , 'Kelly', 'Nate', 'Jose'];
+
+    // // $.each(myArr, function(i, val) {
+    // //     $('#users').append('<li>'+val+'</li>');
+    // // });
+
+    // var newArr = $('ul#list li').toArray();
+    // $.each(myArr, function(i, val) {
+    //       console.log(val.innerHTML);
+    // });
