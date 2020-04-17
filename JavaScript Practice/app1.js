991c7d4f0e0bc53364225ab2fe20e48f733ac931
Anthony <anthony.smith1760@gmail.com>
2020-04-17 12:18:19 -0400

    Variables

---
+++ JavaScript Practice/app1.js
@@ -0,0 +1,20 @@
+var name = 'John Doe';
+console.log(name);
+name='Steve Smith';
+console.log(name);
+
+//Init Var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// letters, numbers ,_ $
+//Can not start with a number
+
+
+//Multiword Variables
+var firstName = 'John'; // Camel Case
+var first_name = 'Sara'; // Underscore
+var FirstName = 'Tom'; // Pascal Case
+var firstname;
