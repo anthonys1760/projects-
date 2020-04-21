976dd52b6d68b7f27360f189bd69286816e78d7d
Anthony <anthony.smith1760@gmail.com>
2020-04-21 12:17:11 -0400

    Switches

--- JavaScript Practice/app2.js
+++ JavaScript Practice/app2.js
@@ -382,3 +382,44 @@ const id = 100;
 //     console.log('Correct');
 // else 
 //     console.log('Incorrect');
+
+// const color = 'yellow';
+
+// switch(color) {
+//     case 'red':
+//         console.log('Color is red');
+//         break;
+//     case 'blue':
+//         console.log('Color is blue');
+//         break;
+//     default:
+//         console.log('Color is not red or blue');
+// }
+
+// let day;
+
+// switch(new Date().getDay()) {
+//     case 0:
+//         day = 'Sunday';
+//         break;
+//     case 1:
+//         day = 'Monday';
+//         break;
+//     case 2:
+//         day = 'Tueday';
+//         break;
+//     case 3:
+//         day = 'Wednesday';
+//         break;
+//     case 4:
+//         day = 'Thursday';
+//         break;
+//     case 5:
+//         day = 'Friday';
+//         break;
+//     case 6:
+//         day = 'Saturday';
+//         break;
+// }
+
+// console.log(`Today is ${day}`);
