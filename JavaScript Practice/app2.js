ff4453bc448e600a97baf4821817338732bb9443
Anthony <anthony.smith1760@gmail.com>
2020-04-21 10:26:48 -0400

    String Methods and Concatenation

--- JavaScript Practice/app2.js
+++ JavaScript Practice/app2.js
@@ -37,30 +37,94 @@
 // console.log(sum);
 // console.log(typeof sum);
 
-const num1 = 100;
-const num2 = 50;
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// //Simple Math with numbers
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// //Math Object
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2, 33, 4, 1, 55, 6);
+// val = Math.max(2, 33, 4, 1, 55, 6);
+// val = Math.random(Math.floor() * 20 + 1);
+
+
+
+// console.log(val);
+
+
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 32;
+const str = 'Hello there my name is Anthony';
+const tags = 'web design, web development, programming';
+
 let val;
 
-//Simple Math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+val = firstName + lastName;
 
-//Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2, 33, 4, 1, 55, 6);
-val = Math.max(2, 33, 4, 1, 55, 6);
-val = Math.random(Math.floor() * 20 + 1);
+//Concatention
+val = firstName + ' ' + lastName;
 
+//Append
+val = 'Brad ';
+val += 'Traversy';
+
+val = 'Hello, my name is  ' + firstName + ' and I am ' + age;
+
+//Escaping 
+val = "That's awesome, I can't wait";
+
+// Lenth 
+val = firstName.length;
+
+//Concat
+val = firstName.concat(' ', lastName);
+
+//Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('1');
+
+//Char at
+val = firstName.charAt('2');
+//Get Last character
+val = firstName.charAt(firstName.length - 1);
+
+// Substring()
+
+val = firstName.substring(0, 4);
+
+//Slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+//split 
+val = str.split(' ');
+val = tags.split(',');
+
+//replace()
+val = str.replace('Anthony', 'Tony');
+
+//includes()
+val = str.includes('foo');
 
 
 console.log(val);
