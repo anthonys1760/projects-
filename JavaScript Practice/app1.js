377e2e9079e4a89bf5bc5fd448729083da4dc7ec
Anthony <anthony.smith1760@gmail.com>
2020-04-21 10:00:57 -0400

    Commit

--- JavaScript Practice/app1.js
+++ JavaScript Practice/app1.js
@@ -1,20 +1,45 @@
-var name = 'John Doe';
-console.log(name);
-name='Steve Smith';
-console.log(name);
+// var name = 'John Doe';
+// console.log(name);
+// name='Steve Smith';
+// console.log(name);
 
-//Init Var
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
+// //Init Var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
 
-// letters, numbers ,_ $
-//Can not start with a number
+// // letters, numbers ,_ $
+// //Can not start with a number
 
 
-//Multiword Variables
-var firstName = 'John'; // Camel Case
-var first_name = 'Sara'; // Underscore
-var FirstName = 'Tom'; // Pascal Case
-var firstname;
+// //Multiword Variables
+// var firstName = 'John'; // Camel Case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal Case
+// var firstname;
+
+
+// CONST
+const name = 'John';
+ console.log(name);
+
+ const person = {
+     name: 'john',
+     age: 30
+ }
+
+ person.name = 'Sara';
+ person.age = 32;
+
+ const number = [1, 2, 3, 4, 5];
+numbers.push(6);
+ console.log(numbers);
+
+ //PRIMITIVE
+
+ //STRING
+
+ const name="John Doe";
+
+ console.log(typeof name);
