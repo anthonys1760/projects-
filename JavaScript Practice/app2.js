6182901c0ab20df2f89b5d30b9c08f6d0fe6679b
Anthony <anthony.smith1760@gmail.com>
2020-04-21 10:12:14 -0400

    Numbers and the Math Object

--- JavaScript Practice/app2.js
+++ JavaScript Practice/app2.js
@@ -1,40 +1,67 @@
+// let val;
+
+// // Number to string
+// val = String(555);
+// val = String(4 + 4);
+// val = String(true);
+// val = String(new Date());
+// //Array to string
+// val = String([1, 2, 3, 4]);
+
+// //toString()
+// val = (5).toString();
+// val = (true).toString();
+
+// //String to number
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('hello');
+// val = Number([1, 2, 3, 4]);
+
+// val = parseInt('100.30');
+// val = parseFloat('100.30');
+
+
+// // //output
+// // console.log(val);
+// // console.log(typeof val);
+// // //console.log(val.length);
+// // console.log(val.toFixed(2));
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = val1 + val2;
+
+// console.log(sum);
+// console.log(typeof sum);
+
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-val = String(555);
-val = String(4 + 4);
-val = String(true);
-val = String(new Date());
-//Array to string
-val = String([1, 2, 3, 4]);
+//Simple Math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-//toString()
-val = (5).toString();
-val = (true).toString();
-
-//String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1, 2, 3, 4]);
-
-val = parseInt('100.30');
-val = parseFloat('100.30');
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6);
+val = Math.max(2, 33, 4, 1, 55, 6);
+val = Math.random(Math.floor() * 20 + 1);
 
 
-// //output
-// console.log(val);
-// console.log(typeof val);
-// //console.log(val.length);
-// console.log(val.toFixed(2));
 
-const val1 = String(5);
-const val2 = 6;
-const sum = val1 + val2;
-
-console.log(sum);
-console.log(typeof sum);
-
+console.log(val);
 
