2c3966fbc04ceac1218d57604b276bfbed0f59c2
Anthony <anthony.smith1760@gmail.com>
2020-04-21 10:56:23 -0400

    Template Literals

--- JavaScript Practice/app2.js
+++ JavaScript Practice/app2.js
@@ -66,66 +66,96 @@
 // console.log(val);
 
 
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 32;
-const str = 'Hello there my name is Anthony';
-const tags = 'web design, web development, programming';
+// const firstName = 'William';
+// const lastName = 'Johnson';
+// const age = 32;
+// const str = 'Hello there my name is Anthony';
+// const tags = 'web design, web development, programming';
 
-let val;
+// let val;
 
-val = firstName + lastName;
+// val = firstName + lastName;
 
-//Concatention
-val = firstName + ' ' + lastName;
+// //Concatention
+// val = firstName + ' ' + lastName;
 
-//Append
-val = 'Brad ';
-val += 'Traversy';
+// //Append
+// val = 'Brad ';
+// val += 'Traversy';
 
-val = 'Hello, my name is  ' + firstName + ' and I am ' + age;
+// val = 'Hello, my name is  ' + firstName + ' and I am ' + age;
 
-//Escaping 
-val = "That's awesome, I can't wait";
+// //Escaping 
+// val = "That's awesome, I can't wait";
 
-// Lenth 
-val = firstName.length;
+// // Lenth 
+// val = firstName.length;
 
-//Concat
-val = firstName.concat(' ', lastName);
+// //Concat
+// val = firstName.concat(' ', lastName);
 
-//Change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
+// //Change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
 
-val = firstName[2];
+// val = firstName[2];
 
-val = firstName.indexOf('l');
-val = firstName.lastIndexOf('1');
+// val = firstName.indexOf('l');
+// val = firstName.lastIndexOf('1');
 
-//Char at
-val = firstName.charAt('2');
-//Get Last character
-val = firstName.charAt(firstName.length - 1);
+// //Char at
+// val = firstName.charAt('2');
+// //Get Last character
+// val = firstName.charAt(firstName.length - 1);
 
-// Substring()
+// // Substring()
 
-val = firstName.substring(0, 4);
+// val = firstName.substring(0, 4);
 
-//Slice()
-val = firstName.slice(0, 4);
-val = firstName.slice(-3);
+// //Slice()
+// val = firstName.slice(0, 4);
+// val = firstName.slice(-3);
 
-//split 
-val = str.split(' ');
-val = tags.split(',');
+// //split 
+// val = str.split(' ');
+// val = tags.split(',');
 
-//replace()
-val = str.replace('Anthony', 'Tony');
+// //replace()
+// val = str.replace('Anthony', 'Tony');
 
-//includes()
-val = str.includes('foo');
+// //includes()
+// val = str.includes('foo');
 
 
-console.log(val);
+// console.log(val);
 
+
+
+// const name = 'John';
+// const age = 30;
+// const job = 'Web Developer';
+// const city = 'Miami';
+// let html;
+
+
+// // html = '<ul>' +
+// //        '<li>Name ' + name + '</li>' +
+// //        '<li>Age ' + age + '</li>' +
+// //        '<li>Job ' + job + '</li>' +
+// //        '<li>City ' + city + '</li>';
+
+// //with template strings (es6)
+// html = `
+//     <ul>
+//         <li>Name: ${name}</li>
+//         <li>Age: ${age}</li>
+//         <li>Job ${job}</li>
+//         <li>City: ${city}</li>
+//     </ul>
+// `;
+
+
+// document.body.innerHTML = html;
+
+
+//Without template strings (es5)
