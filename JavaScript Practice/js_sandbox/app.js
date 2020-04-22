5d4ea2c2946ce3203f42de4f43415a029f70ed39
Anthony <anthony.smith1760@gmail.com>
2020-04-22 09:13:29 -0400

    Mouse Events

--- JavaScript Practice/js_sandbox/app.js
+++ JavaScript Practice/js_sandbox/app.js
@@ -304,3 +304,39 @@
 
 //   console.log(val);
 // }
+
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
+
+//Click
+// clearBtn.addEventListener('click', runEvent);
+//Double Click
+// clearBtn.addEventListener('dblClick', runEvent);
+//MouseDown
+// clearBtn.addEventListener('mousedown', runEvent);
+//MouseUp
+// clearBtn.addEventListener('mouseup', runEvent);
+//MouseEnter
+// card.addEventListener('mouseenter', runEvent);
+//MouseLeave
+// clearBtn.addEventListener('mouseenter', runEvent);
+// //MouseLeave
+// clearBtn.addEventListener('mouseleave', runEvent);
+// //MouseLeave
+// clearBtn.addEventListener('mouseover', runEvent);
+// //MouseLeave
+// clearBtn.addEventListener('mouseout', runEvent);
+//Mousemove
+// card.addEventListener('mousemove', runEvent);
+
+
+
+//Event Handler
+// function runEvent(e) {
+//   console.log(`EVENT TYPE: ${e.type}`);
+
+//   heading.textContent = `MouseX ${e.offsetX} MouseY: ${e.offsetY}`;
+
+//   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+// }
