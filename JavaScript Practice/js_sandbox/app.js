d4999ffdeb841cede0f4886f9d0ca8f7d7c3ebc8
Anthony <anthony.smith1760@gmail.com>
2020-04-22 09:58:10 -0400

    Keyboard & Input Events

--- JavaScript Practice/js_sandbox/app.js
+++ JavaScript Practice/js_sandbox/app.js
@@ -340,3 +340,54 @@
 
 //   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
 // }
+
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+const select = document.querySelector('select');
+//Clear Input
+taskInput.value = '';
+
+// form.addEventListener('submit', runEvent);
+
+//Keyup
+// taskInput.addEventListener('keyup', runEvent);
+
+//Keypress
+// taskInput.addEventListener('keypress', runEvent);
+
+//Keydown
+// taskInput.addEventListener('keydown', runEvent);
+
+//Focus
+// taskInput.addEventListener('focus', runEvent);
+
+//Blur
+// taskInput.addEventListener('blur', runEvent);
+
+//Cut
+// taskInput.addEventListener('cut', runEvent);
+
+//Paste
+// taskInput.addEventListener('paste', runEvent);
+
+// taskInput.addEventListener('input', runEvent);
+
+//Change
+select.addEventListener('change', runEvent);
+
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  // console.log(e.target.value);
+
+  // heading.innerText = e.target.value;
+
+
+  //Get input value
+  
+  // console.log(taskInput.value);
+
+  // e.preventDefault();
+}
+
